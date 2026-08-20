443082a2da3ce4903f7ede5b2ee074f2ffe7453b
101dhruvvyas <101.dhruv.vyas@gmail.com>
2026-08-20 14:14:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def load_and_train():
 
     # Use only 30% of data for training to stay within memory limits
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y_encoded, test_size=0.7, random_state=42
+        X, y_encoded, test_size=0.2, random_state=42
     )
 
     # Smaller forest to reduce memory
